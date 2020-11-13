99a7c5eea58b9529e8d88d1b2acbc4461bc5ef67
Łukasz Flis <luk8426@wp.pl>
2020-11-13 22:48:07 +0100

    Main class with implemented log4j

---
+++ src/main/java/vehicle/Main.java
@@ -0,0 +1,12 @@
+package vehicle;
+
+
+import org.apache.log4j.*;
+
+public class Main {
+    private final static Logger log = LogManager.getLogger(Main.class);
+    public static void main(String[] args) {
+
+        log.info("Entering application.");
+    }
+}
